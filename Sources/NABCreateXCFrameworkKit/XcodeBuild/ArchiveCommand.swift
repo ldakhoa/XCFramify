58ab254752d3dabfc8bbab96d2c13aa870547f6f
Khoa Le <ldakhoa1308@gmail.com>
2023-03-03 02:13:48 +0700

    Feat: Add archive command

---
+++ Sources/NABCreateXCFrameworkKit/XcodeBuild/ArchiveCommand.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct ArchiveCommand: XcodeBuildCommand {
+    // MARK: - Dependencies
+
+    var project: Project
+    var buildConfiguration: BuildConfiguration
+    var sdk: SDK
+
+    // MARK: - XcodeBuildCommand
+
+    let subCommand: String = "archive"
+
+    var options: [XcodeBuildOption] {
+        [
+            ("project", project.projectPath.path),
+            ("configuration", buildConfiguration.settingsValue),
+            ("scheme", project.scheme),
+            ("archivePath", xcArchivePath.path),
+            ("destination", sdk.destination)
+        ].map(XcodeBuildOption.init(key:value:))
+    }
+
+    var environmentVaribles: [XcodeBuildEnvironmentVariable] {
+        [
+            ("BUILD_DIR", project.workspaceDirectory.path),
+            ("SKIP_INSTALL", "NO")
+        ].map(XcodeBuildEnvironmentVariable.init(key:value:))
+    }
+
+    // MARK: - Private
+
+    private var xcArchivePath: URL {
+        buildXCArchivePath(project: project, sdk: sdk)
+    }
+}
