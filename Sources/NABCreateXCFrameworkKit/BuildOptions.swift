1d33f7e98a1722f675c2730a3046bea0f05a5bc1
Khoa Le <ldakhoa1308@gmail.com>
2023-03-03 00:11:02 +0700

    Add docs for BuildOptions

--- Sources/NABCreateXCFrameworkKit/BuildOptions.swift
+++ Sources/NABCreateXCFrameworkKit/BuildOptions.swift
@@ -1,7 +1,19 @@
 import Foundation
 import OrderedCollections
 
+/// A struct representing build options for a framework.
 struct BuildOptions: Hashable, Codable {
+
+    /// Initializes a new instance of `BuildOptions`.
+    /// - Parameters:
+    ///   - buildConfiguration: The build configuration used to build the framework.
+    ///   - isDebugSymbolsEmbedded: A flag indicating whether to embed debug symbols in the built framework.
+    ///   - frameworkType: The type of framework to build.
+    ///   - sdks: An ordered set of `SDK`s to build the framework for.
+    ///   - extraFlags: Extra build flags to pass to the build system.
+    ///   - extraBuildParameters: Extra build parameters to pass to the build system.
+    ///   - enableLibraryEvolution: A flag indicating whether to enable library evolution for the built framework.
+    ///   - enableBuildLibraryForDistribution: A flag indicating whether to build a library for distribution.
     init(
         buildConfiguration: BuildConfiguration,
         isDebugSymbolsEmbedded: Bool,
@@ -10,8 +22,7 @@ struct BuildOptions: Hashable, Codable {
         extraFlags: ExtraFlags?,
         extraBuildParameters: ExtraBuildParameters?,
         enableLibraryEvolution: Bool,
-        enableBuildLibraryForDistribution: Bool,
-        enableSkipInstall: Bool
+        enableBuildLibraryForDistribution: Bool
     ) {
         self.buildConfiguration = buildConfiguration
         self.isDebugSymbolsEmbedded = isDebugSymbolsEmbedded
@@ -21,29 +32,47 @@ struct BuildOptions: Hashable, Codable {
         self.extraBuildParameters = extraBuildParameters
         self.enableLibraryEvolution = enableLibraryEvolution
         self.enableBuildLibraryForDistribution = enableBuildLibraryForDistribution
-        self.enableSkipInstall = enableSkipInstall
     }
 
+    /// The build configuration used to build the framework.
     var buildConfiguration: BuildConfiguration
-    var isDebugSymbolsEmbedded: Bool
-    var frameworkType: FrameworkType
-    var sdks: OrderedSet<SDK>
-    var extraFlags: ExtraFlags?
-    var extraBuildParameters: ExtraBuildParameters?
-    var enableLibraryEvolution: Bool
-    var enableBuildLibraryForDistribution: Bool
-    var enableSkipInstall: Bool
-}
 
+    /// A flag indicating whether to embed debug symbols in the built framework.
+    var isDebugSymbolsEmbedded: Bool
+
+    /// The type of framework to build.
+    var frameworkType: FrameworkType
+
+    /// An ordered set of `SDK`s to build the framework for.
+    var sdks: OrderedSet<SDK>
+
+    /// Extra build flags to pass to the build system.
+    var extraFlags: ExtraFlags?
+
+    /// Extra build parameters to pass to the build system.
+    var extraBuildParameters: ExtraBuildParameters?
+
+    /// A flag indicating whether to enable library evolution for the built framework.
+    var enableLibraryEvolution: Bool
+
+    /// A flag indicating whether to build a library for distribution.
+    var enableBuildLibraryForDistribution: Bool
+}
+/// A structure for specifying extra build flags.
 public struct ExtraFlags: Hashable, Codable {
+    /// An array of additional C compiler flags.
     var cFlags: [String]?
+    /// An array of additional C++ compiler flags.
     var cxxFlags: [String]?
+    /// An array of additional Swift compiler flags.
     var swiftFlags: [String]?
+    /// An array of additional linker flags.
     var linkerFlags: [String]?
 }
 
 public typealias ExtraBuildParameters = [String: String]
 
+/// An enumeration of build configurations.
 public enum BuildConfiguration: String, Codable {
     case debug
     case release
@@ -56,11 +85,13 @@ public enum BuildConfiguration: String, Codable {
     }
 }
 
+/// An enumeration of the types of frameworks that can be built.
 public enum FrameworkType: String, Codable {
     case dynamic
     case `static`
 }
 
+/// An enumeration of the supported SDKs.
 public enum SDK: String, Codable {
     case iOS
     case iOSSimulator
@@ -77,7 +108,8 @@ public enum SDK: String, Codable {
             return nil
         }
     }
-
+    
+    /// Returns a set of SDKs for simulators based on the current SDK.
     func extractForSimulators() -> Set<SDK> {
         switch self {
         case .iOS: return [.iOS, .iOSSimulator]
